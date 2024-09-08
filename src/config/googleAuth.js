466a95b20410df42c045e48bfaf09acef000528e
Abhishek kumar <sahaabhishek443@gmail.com>
2024-09-08 23:56:22 +0530

    uri update

--- src/config/googleAuth.js
+++ src/config/googleAuth.js
@@ -9,8 +9,7 @@ const GoogleOAuth = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL:
-          'https://kiitmart-backend.onrender.com/api/v1/user/auth/google/callback',
+        callbackURL: '/api/v1/user/auth/google/callback',
         scope: ['profile', 'email'],
         passReqToCallback: true,
       },
