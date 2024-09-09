6bfbde425d9aa93acb090b6c70ee1eb7588a5013
Abhishek kumar <sahaabhishek443@gmail.com>
2024-09-10 02:03:31 +0530

    admin

--- src/middleware/isAdmin.middleware.js
+++ src/middleware/isAdmin.middleware.js
@@ -2,7 +2,10 @@ import { ErrorHandler } from '../uttils/errorhandler.middleware.js'
 
 const isAuthorised = (req, res, next) => {
   try {
-    if (req.user.email === '2105598@kiit.ac.in') {
+    if (
+      req.user.email === '2105598@kiit.ac.in' ||
+      req.user.email === '21051449@kiit.ac.in'
+    ) {
       return next()
     }
     throw new ErrorHandler('Unauthorized: User is not admin', 500)
--- src/models/category.model.js
+++ src/models/category.model.js
@@ -4,6 +4,8 @@ const CategorySchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
+    set: (v) => v.charAt(0).toUpperCase() + v.slice(1),
   },
 })
 export const Category = mongoose.model('Category', CategorySchema)
