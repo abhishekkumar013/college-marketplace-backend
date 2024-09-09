b880baa7d5c27756d85ac5708f3fcf4bbd9f6860
Abhishek kumar <sahaabhishek443@gmail.com>
2024-09-09 11:17:14 +0530

    f

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.use(
     }),
     cookie: {
       maxAge: 14 * 24 * 60 * 60 * 1000,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'dev',
       httpOnly: true,
       sameSite: 'none',
     },
